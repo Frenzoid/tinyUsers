070a5e144df56149634e23bf1530f9009dcae41f
Frenzoid <saassabau100@gmail.com>
2019-11-15 13:16:36 +0100

    added toasts and refresher, also better error management

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { backendapi } from '../config/constats';
 import IResponseModel from '../interfaces/IResponseModel';
+import IUser from '../interfaces/IUser';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,21 @@ export class UserService {
 
   constructor(public http: HttpClient) { }
 
-  getUsers(): Observable<IResponseModel> {
-    return this.http.get<IResponseModel>(backendapi + 'users');
+  getUsers(): Observable<IUser[]> {
+    return new Observable((observer) => {
+
+      this.http.get<IResponseModel>(backendapi + 'users').subscribe(
+      (reply) => {
+        if (reply.errors.critical) {
+          console.error(reply);
+          observer.error(reply.errors.messages);
+        }
+        observer.next(reply.data);
+      }, (err: HttpErrorResponse) => {
+        console.error(err);
+        observer.error([err.message]);
+      });
+
+    });
   }
 }
--- src/app/users/users.page.ts
+++ src/app/users/users.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import IUser from '../interfaces/IUser';
 import { UserService } from '../services/user.service';
-import IResponseModel from '../interfaces/IResponseModel';
+import { IonRefresher } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-users',
@@ -10,12 +11,36 @@ import IResponseModel from '../interfaces/IResponseModel';
 })
 export class UsersPage implements OnInit {
 
-  constructor(public userService: UserService) { }
-  users: IUser;
+  constructor(public userService: UserService, public toastController: ToastController) { }
+  @ViewChild('refresherRef', {static: false}) refresherRef: IonRefresher;
+
+  users: IUser[];
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((reply: IResponseModel) => {
-      this.users = reply.data;
+    this.userService.getUsers().subscribe(
+    (users: IUser[]) => {
+      this.users = users;
+    }, (err: string[]) => {
+      this.showToastError(err);
     });
+  }
+
+  doRefresh() {
+    this.userService.getUsers().subscribe(
+    (users: IUser[]) => {
+      this.users = users;
+      this.refresherRef.complete();
+    }, (err: string[]) => {
+      this.showToastError(err);
+      this.refresherRef.complete();
+    });
+  }
+
+  async showToastError(err) {
+    const toast = await this.toastController.create({
+      message: err,
+      duration: 5000
+    });
+    toast.present();
   }
 }
